7e22d989398f88bf70f26e2fd8af43db35f306e8
QinyueFei <qyfei@pku.edu.cn>
2021-12-11 16:03:28 +0800

    Dynamical model and analysis

---
+++ CODES/Dynamics/__init__.py
@@ -0,0 +1,3 @@
+## Files included in this document are used to do dynamical analysis.
+## asym_drift.py shows the 'real' circular motions or rotation curves after asymmetric drift corrections.
+## models.py describes the theoretical and numerical rotation velocities with deprojected models.
