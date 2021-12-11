8ba4b309fe7dcc01ede289778694a1d29b0fb52a
QinyueFei <qyfei@pku.edu.cn>
2021-12-11 16:05:19 +0800

    Barolo analysis

---
+++ CODES/Barolo_analyse/__init__.py
@@ -0,0 +1,4 @@
+## Files included in this document are used to analyse the Barolo fitting result
+## moment.py is used to visual and analyze the Barolo fitting results and real observations.
+## parameters.py is used to load the parameters from Barolo model.
+## pv_diagram.py is used to compare the position-velocity diagram of observations and models.
