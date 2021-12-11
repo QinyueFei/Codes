6b40f33b8b6e516374b47f1ce55fb2d173d4993a
QinyueFei <qyfei@pku.edu.cn>
2021-12-11 16:25:17 +0800

    Analyze the observational result

--- CODES/__init__.py
+++ CODES/__init__.py
@@ -1,1 +1,6 @@
-import matplotlib.pyplot as plt
+## Try to use CODES developed by us and study the galaxy properties
+## disk_instability.py: calculate the Toomre Q parameter for galaxy disk, study whether this is a stable or turbulent disk
+## Dynamical_analysis.py: fit the corrected rotation curve with our mass model
+## p_turb.py: calculate the turbulent pressure
+## rc_fit.py: fit the corrected rotation cufve
+## Sigma_vs_sigma.py: compare the gas surface density (Sigma) and velocity dispersion (sigma) and study the dynamical state of disk gas
