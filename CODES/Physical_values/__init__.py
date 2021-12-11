c294dda638d3eed61fda0a2a17c20d001b0899e2
QinyueFei <qyfei@pku.edu.cn>
2021-12-11 16:09:31 +0800

    extract the physical properties of gas in galaxies

--- CODES/Physical_values/__init__.py
+++ CODES/Physical_values/__init__.py
@@ -1,10 +1,11 @@
 ## Codes in this module are developed to extract the physical conditions from observations.
 
-## surface_density.py: derive the surface density of gas through moment 0 maps with estimation of a_CO, line ratio and other parameters.
 
 ## pycprops: Codes that are used to identify giant molecular clouds (GMC) in disk galaxies, a popular paper that use: Sun+2018. See the detailed algorithm in Rosolowsky & Leroy+2008.
 
+## surface_density.py: derive the surface density of gas through moment 0 maps with estimation of a_CO, line ratio and other parameters.
 ## clouds.py: finds clouds eith cprops algorithm, the identification of clouds are listed in clouds document
 ## show_clouds.py: overplot the clouds position and shape on moment 0 maps
 ## clouds_properties.py: obtain the properties of each clouds
-
+## clouds_spectral.py: extract the spectra of clouds at a certain position
+## stellar_properties.py: add star properties from other band analysis
