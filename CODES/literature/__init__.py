278e930e646943a596b651efe0f4bc5d4e0f12f3
QinyueFei <qyfei@pku.edu.cn>
2021-12-11 16:06:32 +0800

    Analysing literature results

--- CODES/literature/__init__.py
+++ CODES/literature/__init__.py
@@ -1,7 +1,7 @@
 ## This module contains data adopted from previous studies, including a lot of relationships.
-
+## load.py is used to loading parameters from previous works
 import matplotlib.pyplot as plt
 plt.rc('text')
-plt.rc('font', family='serif', size=15)
+plt.rc('font', family='serif', size=25)
 plt.rc('xtick', direction='in', top=True)
 plt.rc('ytick', direction='in', right=True)
