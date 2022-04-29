2da450612bcd00e6c8f2c96ca4910807d0489ab4
QinyueFei <qyfei@pku.edu.cn>
2022-04-29 17:47:54 +0800

    NOEMA detection

--- CODES/NOEMA_observation/detection.py
+++ CODES/NOEMA_observation/detection.py
@@ -36,13 +36,19 @@ def beam(HDU, XPOS, YPOS, col, cellsize):
 
 # %%
 ## Load data ##
-path = "/media/qyfei/f6e0af82-2ae6-44a3-a033-f66b47f50cf4/NOEMA/w20cf/w20cf_origin/w20cf001/"
-name = "F08238+0752"
-file = name + "_CO32.fits"
-file_mom0 = name + "_CO32_mom0.fits"
-file_mom1 = name + "_CO32_mom1.fits"
-file_mom2 = name + "_CO32_mom2.fits"
+# path = "/media/qyfei/f6e0af82-2ae6-44a3-a033-f66b47f50cf4/NOEMA/w20cf/w20cf_origin/w20cf001/"
+# # name = "F08238+0752"
+# path = "/media/qyfei/f6e0af82-2ae6-44a3-a033-f66b47f50cf4/ALMA/Type1AGN/PG_quasars/PG0923/data"#Re_Re_calibration/product"
+# # file = "/PG1244+026_CO32_clean_mom1.fits"
+# name = "/PG0923+129"
+# file = name + "_CO21.final.image.fits"
+# file_mom0 = name + "_CO21_final_image_mom0.fits"
+# file_mom1 = name + "_CO32_10km_clean_mom1.fits"
+# file_mom2 = name + "_CO32_10km_clean_mom2.fits"
 
+path = "/media/qyfei/f6e0af82-2ae6-44a3-a033-f66b47f50cf4/ALMA/PG0050+124/CO21_combine/combine/line"
+file = "/PG0050_CO21-combine-line-10km-mosaic.fits"
+file_mom0 = "/PG0050_CO21-combine-line-10km-mosaic-mom0.fits"
 
 hdu = fits.open(path+file)[0]
 
@@ -53,50 +59,74 @@ pix_size = delt*u.deg.to('arcsec')
 beam_area = np.pi*bmaj*bmin/delt**2/4/np.log(2)
 
 wcs = WCS(hdu.header)
+
 # %%
 # find source, estimate spectral resolution
 cube_data = hdu.data
 hdr = hdu.header
 CO32_data = SpectralCube.read(path+file)
-velo = CO32_data.spectral_axis
-restfreq = hdr['RESTFREQ']/1e9
-freq = (velo/c.c).si*restfreq+restfreq
+# velo = CO32_data.spectral_axis
+# restfreq = hdr['RESTFREQ']/1e9
+# freq = (velo/c.c).si*restfreq+restfreq
+freq = CO32_data.spectral_axis.value/1e9
 
 hdu = fits.open(path+file_mom0)[0]
-mom0 = hdu.data[0]
-coordinates = peak_local_max(mom0, min_distance=100)
-mom1 = fits.open(path+file_mom1)[0].data[0]
-mom2 = fits.open(path+file_mom2)[0].data[0]
+mom0 = hdu.data[0][0]
+# coordinates = peak_local_max(mom0, min_distance=100)
+# mom1 = fits.open(path+file_mom1)[0].data[0][0]
+# mom2 = fits.open(path+file_mom2)[0].data[0][0]
 
-print(coordinates)
-print(velo[1]-velo[2])
+
+coordinates = [[397, 400]] #for PG0050
+# coordinates = [[88, 89]] #for PG0923
+# coordinates = [[91, 90]] #for PG1011
+# coordinates = [[85, 85]] #for PG1244
+
+
+
+
+# print(coordinates)
+# print(velo[1]-velo[2])
+
+from astropy.coordinates import SkyCoord
+c = SkyCoord('00h53m34.94s', '+12d41m36.2s', frame='icrs')
+c = SkyCoord('09h26m03.29s', '+12d44m03.6s', frame='icrs')
+c = SkyCoord('10h14m20.69s', '-04d18m40.5s', frame='icrs')
+c = SkyCoord('12h46m35.25s', '+02d22m08.8s', frame='icrs')
+
+from astropy.wcs import WCS
+w = WCS(hdu.header)
+
+# c.ra - w[0,0].pixel_to_world([84,85,86,87,88],85).ra
 
 # %%
 ## Plot moment 0
 pos_cen = coordinates[0]
 yy, xx = np.indices([hdr['NAXIS1'], hdr['NAXIS2']],dtype='float')
 radius = ((yy-pos_cen[0])**2+(xx-pos_cen[1])**2)**0.5
-rad = 1.0
+rad = 0.55
 ring = abs(rad/pix_size)
 
-size = abs(4/pix_size)
+size = abs(10/pix_size)
 mom0_rms = sigma_clipped_stats(mom0)[-1]
+# print(mom0_rms)
+mom0_rms = 0.055
 print("The rms noise of moment 0 is:", mom0_rms, "Jy/beam km/s")
 mom0_level = np.array([-1,1,2,4,8,16,32])*2*mom0_rms
 
 fig = plt.figure(figsize=(8,10))
-ax = plt.subplot(projection=wcs[0])
+ax = plt.subplot(projection=wcs[0,0])
 
 im = ax.imshow(mom0, cmap='jet', origin='lower')
 cp,kw = colorbar.make_axes(ax, pad=0.01, aspect=18, location='top')
 cb = plt.colorbar(im, cax=cp, orientation='horizontal', ticklocation='top')
-cb.set_label(name+' CO(3-2) [Jy/beam$\cdot$km/s]')
+cb.set_label('PG1244+026'+' CO(3-2) [Jy/beam$\cdot$km/s]')
 ax.contour(mom0, mom0_level, colors=['k'])
 
 circ = matplotlib.patches.Circle((pos_cen[1], pos_cen[0]), ring, fill=False, edgecolor='m')
 ax.add_artist(circ)
 
-rec_size = abs(bmaj/delt)*1.25
+rec_size = abs(bmaj/delt)*1.5
 rec = matplotlib.patches.Rectangle((pos_cen[1]-size, pos_cen[0]-size), rec_size, rec_size, angle=0.0,fill=True, edgecolor='k', facecolor='w', zorder=2)
 ax.add_artist(rec)
 Beam = beam(hdu, pos_cen[1]-size+rec_size/2,pos_cen[0]-size+rec_size/2, 'k', pix_size)
@@ -106,8 +136,9 @@ ax.set_ylim(pos_cen[0]-size, pos_cen[0]+size)
 ax.set_xlabel("R.A. (J2000)", labelpad=0.5)
 ax.set_ylabel("Dec (J2000)", labelpad=-1.0)
 
-ax.scatter(512, 512, marker="+", color='w', s=200, zorder=3)
+ax.scatter(540, 540, marker="+", color='w', s=200, zorder=3)
 ax.scatter(pos_cen[1], pos_cen[0], marker='*', color='k', zorder=3)
+# plt.savefig('/home/qyfei/Desktop/Results/map_visualization/Image/PG_quasars/moment0/PG1244_CO32_mom0.pdf', bbox_inches='tight', dpi=300)
 
 #plt.savefig("/home/qyfei/Desktop/Results/NOEMA_detection/origin/"+name+"_mom0.pdf", bbox_inches="tight", dpi=300)
 
@@ -116,7 +147,7 @@ ax.scatter(pos_cen[1], pos_cen[0], marker='*', color='k', zorder=3)
 ## build the mask and extract the spectrum
 mask = (radius<=ring) & (mom0>=2*mom0_rms)
 hdu = fits.open(path+file)[0]
-cube_data = hdu.data
+cube_data = hdu.data[0]
 hdr = hdu.header
 
 # calculate the total flux within aperture
@@ -127,22 +158,31 @@ flux = np.nansum(spectrum, axis=(1,2))/beam_area*u.Unit('Jy').to('mJy')
 #spectrum = cube_data[:, pos_cen[0], pos_cen[1]]#*mask
 #flux = spectrum*u.Unit('Jy').to('mJy')
 
-sigma = sigma_clipped_stats(flux[112:350])[-1]
+sigma = sigma_clipped_stats(flux[100:230])[-1]
 
 #%%
 ## plot the figure of spectra
 #flux_cen_norm = flux_cen/np.nanmax(flux_cen)
 
-plt.figure(figsize=(8,12))
-grid=plt.GridSpec(5,1,wspace=0,hspace=0)
-ax1=plt.subplot(grid[0:3])
-ax1.step(freq, flux, 'k', label='Whole')
-ax1.fill_between(freq, -sigma, sigma, facecolor='none',hatch='/',linestyle=':',alpha=0.5)
+norm = 1#np.nanmax(flux)
+
+plt.figure(figsize=(8,8))
+# grid=plt.GridSpec(5,1,wspace=0,hspace=0)
+# ax1=plt.subplot(grid[0:3])
+ax1 = plt.subplot(111)
+ax1.step(freq, flux/norm, 'k', label='Whole')
+ax1.fill_between(freq, -sigma/norm, sigma/norm, facecolor='none',hatch='/',linestyle=':',alpha=0.5)
 ax1.set_xlabel('Frequency [$\mathrm{GHz}$]')
 ax1.set_ylabel('Flux [$\mathrm{mJy}$]')
-ax1.legend(loc = 'upper left')
+# ax1.set_ylabel('Normalized flux density')
+# ax1.legend(loc = 'upper left')
 ax1.grid()
-ax1.set_xlim(freq.min(), freq.max())
+
+res_freq = 230.5380/(1+0.061)
+ax1.set_xlim(res_freq-0.4, res_freq+0.4)
+ax1.set_ylim(-3*sigma, np.nanmax(flux)+5*sigma)
+# plt.savefig('/home/qyfei/Desktop/Results/Result/PG0923_central_spectra.pdf', bbox_inches='tight', dpi=300)
+# ax1.set_xlim(freq.min(), freq.max())
 #plt.savefig('/home/qyfei/Desktop/Codes/Result/spectrum.pdf', bbox_inches='tight', dpi=300)
 
 # %%
@@ -177,7 +217,7 @@ def log_probability(para, x, y, yerr):
 
 from scipy.optimize import minimize
 
-x = freq.value
+x = freq#.value
 
 a_g = np.max(flux)
 N = peak_local_max(flux)[0][0]
@@ -207,7 +247,7 @@ plt.xlim(m_ml-3*np.sqrt(8*np.log(2))*s_ml, m_ml+3*np.sqrt(8*np.log(2))*s_ml)
 import emcee
 from multiprocessing import Pool
 
-pos = [a_ml, m_ml, s_ml] + 1e-4 * np.random.randn(32, 3)
+pos = [a_ml, m_ml, 0.045] + 1e-4 * np.random.randn(32, 3)
 nwalkers, ndim = pos.shape
 with Pool() as pool:
     sampler = emcee.EnsembleSampler(nwalkers, ndim, log_probability, args=(x, y, yerr), pool=pool)
@@ -216,25 +256,24 @@ with Pool() as pool:
 # %%
 ## Check fitting
 
-#fig, axes = plt.subplots(3, figsize=(10, 7), sharex=True)
+fig, axes = plt.subplots(3, figsize=(10, 7), sharex=True)
 samples = sampler.get_chain()
 labels = ["A", "m", "sigma"]
-'''for i in range(ndim):
+for i in range(ndim):
     ax = axes[i]
     ax.plot(samples[:, :, i], "k", alpha=0.3)
     ax.set_xlim(0, len(samples))
     ax.set_ylabel(labels[i])
-    ax.yaxis.set_label_coords(-0.1, 0.5)'''
+    ax.yaxis.set_label_coords(-0.1, 0.5)
 
 #axes[-1].set_xlabel("step number")
 
 flat_samples = sampler.get_chain(discard=100, thin=15, flat=True)
 print(flat_samples.shape)
-'''
 import corner
 fig = corner.corner(
     flat_samples, labels=labels#, truths=[]
-)'''
+)
 
 # %%
 ## Output and save parameters
@@ -256,10 +295,10 @@ inds = np.random.randint(len(flat_samples), size=100)
 
 ## Calculate flux
 from scipy.integrate import trapz
-x_test = np.linspace(freq.value[0], freq.value[-1], 1000)
+x_test = np.linspace(freq[0], freq[-1], 1000)
 redshifts = np.zeros(len(flat_samples))
 for i in range(len(flat_samples)):
-    redshift = 345.80/flat_samples[i][1]-1
+    redshift = 230.58/flat_samples[i][1]-1
     redshifts[i] = redshift
 
 print("Estimate redshift:")
@@ -280,15 +319,20 @@ grid=plt.GridSpec(6,1,wspace=0,hspace=0)
 ax1=plt.subplot(grid[0:5])
 ax2=plt.subplot(grid[5:])
 ax1.step(x, y, 'k',label='Spectrum', where='mid')
+
 for ind in inds:
     sample = flat_samples[ind]
     ax1.plot(x_test, Gauss(x_test, sample[0], sample[1], sample[2]), "r", alpha=0.1)
 
-ax1.plot(x_test, Gauss(x_test, para_fit[0], para_fit[1], para_fit[2]), 'r', label='Fit')
-ax1.fill_between(x, -yerr, yerr, facecolor='k',hatch='/',linestyle=':',alpha=0.5, label=r'1$\sigma$ noise')
 w50 = 2*np.log(2)*para_fit[2]#+np.sqrt(2*np.log(2))*para_fit[3]
 ax1.vlines(para_fit[1]-w50, -100,500, 'b', ls='--', label='$W_{50}$')
 ax1.vlines(para_fit[1]+w50, -100,500, 'b', ls='--')
+
+ax1.plot(x_test, Gauss(x_test, para_fit[0], para_fit[1], para_fit[2]), 'r', label='Fit')
+ax1.fill_between(x, -yerr, yerr, facecolor='none',hatch='/',linestyle='-',alpha=0.5, label=r'1$\sigma$ noise')
+
+N = np.where((x>para_fit[1]-3*para_fit[2])&(x<para_fit[1]+3*para_fit[2]))
+ax1.fill_between(x[N], y[N], facecolor='k', step='mid', alpha=0.5, label='Signal')
 
 ax1.hlines(0,-1000,1000,'k',':')
 ax1.set_xlim(para_fit[1]-10*para_fit[2], para_fit[1]+10*para_fit[2])
@@ -298,7 +342,7 @@ ax1.legend(loc='upper left', frameon=False)
 
 res = y - Gauss(x, para_fit[0], para_fit[1], para_fit[2])
 ax2.step(x, res, 'k', where='mid')
-ax2.fill_between(x, -yerr, yerr, facecolor='k',hatch='/',linestyle=':',alpha=0.5)
+ax2.fill_between(x, -yerr, yerr, facecolor='none',hatch='/',linestyle='-',alpha=0.5)
 ax2.hlines(0,-1000,1000,'k',':')
 ax2.set_xlim(para_fit[1]-10*para_fit[2], para_fit[1]+10*para_fit[2])
 ax2.set_ylim(-5*yerr, 5*yerr)
@@ -315,7 +359,7 @@ print(chi2)
 
 # %%
 ckms = (c.c.to('km/s')).value
-velo = (freq.value/345.8*(1+Z[1]) - 1)*ckms
+velo = (freq/230.58*(1+Z[1]) - 1)*ckms
 
 x = velo
 N = peak_local_max(flux)[0][0]
@@ -390,7 +434,7 @@ display(Math(txt))
 parameters.append([FLUX[1], q[0], q[1]])
 parameters.append([Z[1], zs[0], zs[1]])
 
-np.savetxt('/home/qyfei/Desktop/Results/Result/NOEMA_detection/origin/'+name+'_properties.txt', np.array(parameters))
+# np.savetxt('/home/qyfei/Desktop/Results/Result/NOEMA_detection/origin/'+name+'_properties.txt', np.array(parameters))
 
 # %%
 #############################
@@ -401,19 +445,23 @@ plt.figure(figsize=(16, 10))
 grid=plt.GridSpec(6,1,wspace=0,hspace=0)
 ax1=plt.subplot(grid[0:5])
 ax2=plt.subplot(grid[5:])
-ax1.step(x, y, 'k',label='Spectrum', where='mid')
+ax1.step(x, y, color = 'black', label='Spectrum', where='mid')
 for ind in inds:
     sample = flat_samples[ind]
     ax1.plot(x_test, Gauss(x_test, sample[0], sample[1], sample[2]), "r", alpha=0.1)
 
-ax1.plot(x_test, Gauss(x_test, para_fit[0], para_fit[1], para_fit[2]), 'r', label='Fit')
-ax1.fill_between(x, -yerr, yerr, facecolor='k',hatch='/',linestyle=':',alpha=0.5, label=r'1$\sigma$ noise')
 w50 = 2*np.log(2)*para_fit[2]#+np.sqrt(2*np.log(2))*para_fit[3]
-ax1.vlines(para_fit[1]-w50, -100,500, 'b', ls='--', label='$W_{50}$')
-ax1.vlines(para_fit[1]+w50, -100,500, 'b', ls='--')
+ax1.vlines(para_fit[1]-w50, -100,500, 'b', ls='--', lw=2.5, label='$W_{50}$', zorder=3)
+ax1.vlines(para_fit[1]+w50, -100,500, 'b', ls='--', lw=2.5, zorder=3)
+
+ax1.plot(x_test, Gauss(x_test, para_fit[0], para_fit[1], para_fit[2]), 'r', label='Fit')
+ax1.fill_between(x, -yerr, yerr, facecolor='none',hatch='/',linestyle='-',alpha=0.5, label=r'1$\sigma$ noise')
+
+N = np.where((x>para_fit[1]-3*para_fit[2]) & (x<para_fit[1]+3*para_fit[2]))
+ax1.fill_between(x[N], y[N], facecolor='k', step='mid', alpha=0.5, label='Signal')
 
 ax1.hlines(0,-1000,1000,'k',':')
-ax1.set_xlim(para_fit[1]-10*para_fit[2], para_fit[1]+10*para_fit[2])
+ax1.set_xlim(para_fit[1]-10.1*para_fit[2], para_fit[1]+10.1*para_fit[2])
 ax1.set_ylim(-3*yerr, para_fit[0]+5*yerr)
 ax1.set_ylabel('Flux density [mJy]')
 ax1.legend(loc='upper left', frameon=False)
@@ -421,9 +469,9 @@ ax1.legend(loc='upper left', frameon=False)
 
 res = y - Gauss(x, para_fit[0], para_fit[1], para_fit[2])
 ax2.step(x, res, 'k', where='mid')
-ax2.fill_between(x, -yerr, yerr, facecolor='k',hatch='/',linestyle=':',alpha=0.5)
+ax2.fill_between(x, -yerr, yerr, facecolor='none',hatch='/',linestyle='-',alpha=0.5)
 ax2.hlines(0,-1000,1000,'k',':')
-ax2.set_xlim(para_fit[1]-10*para_fit[2], para_fit[1]+10*para_fit[2])
+ax2.set_xlim(para_fit[1]-10.1*para_fit[2], para_fit[1]+10.1*para_fit[2])
 ax2.set_ylim(-5*yerr, 5*yerr)
 ax2.set_xlabel("Velocity [$\mathrm{km\,s^{-1}}$]")
 
@@ -431,6 +479,45 @@ fer = "_aper"
 #fer = "_peak"
 
 #plt.savefig("/home/qyfei/Desktop/Results/Result/NOEMA_detection/origin/"+name+"_spectrum_velo"+fer+".pdf", bbox_inches='tight', dpi=300)
+# plt.savefig("/home/qyfei/Desktop/Results/map_visualization/Image/PG_quasars/moment0/PG1244_CO32_spectrum_10km_resolution.pdf", bbox_inches='tight', dpi=300)
+
+# %%
+
+# CO32_vel = x
+# CO32_flux = y
+CO21_vel = x
+CO21_flux = y
+CO21_err = yerr
+# %%
+
+plt.figure(figsize=(16, 10))
+grid=plt.GridSpec(5,1,wspace=0,hspace=0)
+ax1=plt.subplot(grid[0:5])
+# ax2=plt.subplot(grid[5:])
+ax1.step(CO21_vel, CO21_flux, color = 'blue', ls = '-', label='CO(2-1)', where='mid')
+ax1.errorbar(CO21_vel, CO21_flux, yerr = CO21_err, fmt = 'bo', ms = 0, capsize=0)
+ax1.step(CO32_vel, CO32_flux, color = 'black', ls = '-', label='CO(3-2)', where='mid')
+ax1.errorbar(CO32_vel, CO32_flux, yerr = 3.58, fmt = 'ko', ms = 0, capsize=0)
+# for ind in inds:
+#     sample = flat_samples[ind]
+#     ax1.plot(x_test, Gauss(x_test, sample[0], sample[1], sample[2]), "r", alpha=0.1)
+
+# w50 = 2*np.log(2)*para_fit[2]#+np.sqrt(2*np.log(2))*para_fit[3]
+# ax1.vlines(para_fit[1]-w50, -100,500, 'b', ls='--', lw=2.5, label='$W_{50}$', zorder=3)
+# ax1.vlines(para_fit[1]+w50, -100,500, 'b', ls='--', lw=2.5, zorder=3)
+
+# ax1.plot(x_test, Gauss(x_test, para_fit[0], para_fit[1], para_fit[2]), 'r', label='Fit')
+# ax1.fill_between(x, -yerr, yerr, facecolor='none',hatch='/',linestyle='-',alpha=0.5, label=r'1$\sigma$ noise')
+
+# N = np.where((x>para_fit[1]-3*para_fit[2]) & (x<para_fit[1]+3*para_fit[2]))
+# ax1.fill_between(x[N], y[N], facecolor='k', step='mid', alpha=0.5, label='Signal')
+
+ax1.hlines(0,-1000,1000,'k',':')
+ax1.set_xlim(para_fit[1]-10.1*para_fit[2], para_fit[1]+10.1*para_fit[2])
+ax1.set_ylim(-5*yerr, para_fit[0]+10*yerr)
+ax1.set_ylabel('Flux density [mJy]')
+ax1.legend(loc='upper left', frameon=False)
+# plt.savefig("/home/qyfei/Desktop/Results/map_visualization/Image/PG_quasars/moment0/PG1244_spectra.pdf", bbox_inches='tight', dpi=300)
 
 # %%
 ## Test result
